9be0a6365d5d9493d839a0f0b6628a2a449f0b49
FreedomLouise <freedomaguinaldo@gmail.com>
2025-12-16 12:01:14 +0800

    fifteenth

--- public/dimensions/dimensions.js
+++ public/dimensions/dimensions.js
@@ -1,20 +1,15 @@
 // initialize slides for dimensions section
 
-const sliderView = document.querySelector(".slider-view");
 const slides = document.querySelectorAll(".slider-view img");
 const dots = document.querySelectorAll(".dot");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
-let index = 0;
-
-slides.forEach(function(slide) {
-    sliderView.style.width = `${100 * slides.length}%`;
-});
+let index = 1;
 
 // slide selection for dimensions section
 
-function dotsUpdate() {
+function dotsUpdate(i) {
     dots.forEach(function(dot, i) {
         if (i === index) {
             dot.classList.add("active");
@@ -25,11 +20,16 @@ function dotsUpdate() {
 };
 
 function updateSlider() {
-    sliderView.style.transform = `translateX(-${100 * index}%)`;
+    slides.forEach(function(slide) {
+        slide.style.transform = `translateX(-${100 * index}%)`;
+    });
     dotsUpdate();
 }
 
-updateSlider();
+slides.forEach(function(slide) {
+    slide.style.transform = `translateX(-${100}%)`;
+});
+dotsUpdate(i=1);
 
 nextBtn.addEventListener("click", () => {
     index = (index + 1);
